72ef62b70b5f6840b2191e830d0a5ef79f41e099
Shubhamm5932 <shubhamh2@hexaware.com>
2024-10-01 16:19:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
@@ -11,9 +11,7 @@ st.write(
     """
 )
 
-import streamlit as st
-
-from snowflake.snowpark.functions import col
+#import streamlit as st
 
 name_on_order = st.text_input('name on smoothie:')
 st.write('the name on your smoothie will be:', name_on_order)
@@ -26,6 +24,7 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
     'choose upto five ingredients:'
     , my_dataframe 
+    , max_selections=5
 )
 
 if ingredients_list:
@@ -38,8 +37,8 @@ if ingredients_list:
         
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     #st.write(my_insert_stmt)
     time_to_insert = st.button('submit order')
