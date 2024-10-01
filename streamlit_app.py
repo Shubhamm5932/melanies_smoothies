bccb43fc2d02f77282336b04a381370f1b3e351c
Shubhamm5932 <shubhamh2@hexaware.com>
2024-10-01 18:10:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('your smoothie is ordered! ' + name_on_order + '', icon = "✅")
 
-
+#new section to display fruityvice nutrition information.
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 
